92f2d2e51d2f33733465b12b2c795ad9b9101a15
baguwix <slava2002t12@gmail.com>
2023-07-09 19:27:47 +0300

    Conditions

--- src/main/java/com/epam/conditions/DaysInMonth.java
+++ src/main/java/com/epam/conditions/DaysInMonth.java
@@ -3,7 +3,55 @@ package com.epam.conditions;
 public class DaysInMonth {
 
     public void printDays(int year, int month) {
+        if(year < 0 || month < 1 || month > 12) System.out.println("invalid date");
 
+
+        boolean isLeapYear = false;
+
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    isLeapYear = true;
+                }
+            } else {
+                isLeapYear = true;
+            }
+        }
+
+        int daysInMonth;
+
+        switch (month) {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                daysInMonth = 31;
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                daysInMonth = 30;
+                break;
+            case 2:
+                if (isLeapYear) {
+                    daysInMonth = 29;
+                } else {
+                    daysInMonth = 28;
+                }
+                break;
+            default:
+                daysInMonth = -1;
+                break;
+        }
+
+        if (daysInMonth == -1) {
+            System.out.println("Invalid date");
+        } else {
+            System.out.println("Number of days in " + month + "/" + year + ": " + daysInMonth);
+        }
     }
-
 }
--- src/main/java/com/epam/conditions/TriangleSidesValidator.java
+++ src/main/java/com/epam/conditions/TriangleSidesValidator.java
@@ -4,6 +4,11 @@ public class TriangleSidesValidator {
 
     public void validate(double firstSide, double secondSide, double thirdSide) {
 
+        if(firstSide + secondSide > thirdSide && firstSide + thirdSide > secondSide && secondSide + thirdSide > firstSide){
+            System.out.println("This is a valid triangle");
+        } else {
+            System.out.println("It's not a triangle");
+        }
     }
 
 }
